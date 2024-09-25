89362c48992b2710131fbd32926ff3a01997392c
Qianyu Chen <chen0928@algonquinlive.com>
2024-09-25 17:37:51 -0400

    completed class exercise

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((e) => {
+        console.error("Error fetching data: ", e);
+      });
+  }, []);
+
+  // Item renderer for FlatList
+  const renderItem = ({ item }) => (
+    <View style={styles.todoItem}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.completed}>
+        {item.completed ? "✅ Completed" : "❌ Incomplete"}
+      </Text>
     </View>
+  );
+
+  // Key extractor for FlatList
+  const keyExtractor = (item) => item.id.toString();
+
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <FlatList
+          data={todos}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 16,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  todoItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  completed: {
+    fontSize: 16,
+    color: "#666",
+    marginTop: 8,
   },
 });
